918bdc0ffaccbe72cce014495c880eea14ecface
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2024-04-26 23:08:27 +0300

    ADD autoloading with composer

--- autoloading/index.php
+++ autoloading/index.php
@@ -2,14 +2,21 @@
 // require_once __DIR__ . '/app/Stripe/Stripe.php';
 // require_once __DIR__ . '/app/Paddle/Paddle.php';
 
-spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-    require $path;
-});
+// spl_autoload_register(function ($class) {
+//     $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
+
+require __DIR__ . '/../vendor/autoload.php';
 
 use app\Stripe\Stripe;
 use app\Paddle\Paddle;
 
 
-$s = new Stripe();
-$p = new Paddle();
+// $s = new Stripe();
+// $p = new Paddle();
+
+$id = new \Ramsey\Uuid\UuidFactory();
+// var_dump(get_class_methods($id));
